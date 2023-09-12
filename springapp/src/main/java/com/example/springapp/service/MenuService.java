c0e9a4d0bd9ca07421af7e1dc25e89e9244db200
Iamneo <techadmin@iamneo.ai>
2023-09-12 07:23:42 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/example/springapp/service/MenuService.java
@@ -0,0 +1,29 @@
+package com.example.springapp.service;
+
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.example.springapp.model.MenuItem;
+
+@Service
+public class MenuService {
+
+    private final Map<Integer, MenuItem> menuItems = new HashMap<>();
+    private int nextId = 1;
+
+    
+    public boolean addMenuItem(MenuItem menuItem) {
+        menuItem.setId(nextId++);
+        menuItems.put(menuItem.getId(), menuItem);
+        return true;
+    }
+
+
+    public boolean deleteMenuItem(int id) {
+        return menuItems.remove(id) != null;
+    }
+}
